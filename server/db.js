545837d12e5585efe471675e8a44473330316df1
cssddd3 <cssddd3@gmail.com>
2021-04-16 17:05:30 +0900

    first commit

---
+++ server/db.js
@@ -0,0 +1,8 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://spectra:artceps@cluster0.row7g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
